661ae1c218dd8f0e07d3d18677b7dc2ab169f076
Andrew TOwnsley <atownsley86@gmail.com>
2021-08-21 08:22:29 -0600

    progress bar is filling ehern a pledge button is clicked, now working on incrementing the progress value based on the value of the pledge selected

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const progressFill = document.getElementById('progress-fill');
 const btnContainer = document.querySelector('.btn-container');
 const body = document.querySelector('body');
+let width = 1;
 
 const selectPledgeAmount = (e) => {
     if(e.target !== e.currentTarget) {
         const currentPledge = document.querySelector('.pledge-btn');
+        console.log(e.target.dataset);
         if(currentPledge) {
             currentPledge.classList.remove('active');
             console.log("current pledge btn");
         }
-        const btn = e.target;
-        btn.classList.toggle('active');
+        width++
+        const progressFill = document.getElementById('progress-fill');
+        // const btn = e.target;
+        progressFill.style.width = width + e.target.dataset +  "%";
+        // btn.classList.toggle('active');
         e.stopPropagation();
     }
 }
