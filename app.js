8a73bd7c6a78daf438c058be6c526f133e10cc40
Andrew TOwnsley <atownsley86@gmail.com>
2021-08-22 08:49:23 -0600

    fixed the pledge value error.  The pledgeValue and pledgeTotal were concatenating instead of adding, so they both needed toi be converted to numbers when they were declared, not after addition.  Next is getting the pledgeValue to match the proportion of the progress bar that is being filled.

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 const progressFill = document.getElementById('progress-fill');
 const btnContainer = document.querySelector('.btn-container');
 const body = document.querySelector('body');
-let width = 1;
+let pledgeTotal = 0;
 
 const selectPledgeAmount = (e) => {
     if(e.target !== e.currentTarget) {
         const currentPledge = document.querySelector('.pledge-btn');
-        console.log(e.target.dataset);
         if(currentPledge) {
-            currentPledge.classList.remove('active');
-            console.log("current pledge btn");
+            const progressFill = document.getElementById('progress-fill');
+            let pledgeValue = parseFloat(e.target.dataset.value);
+            progressFill.style.width = parseFloat(pledgeTotal);
+            let newWidth = pledgeTotal += pledgeValue;
+            progressFill.style.width = newWidth + "px";       
+            console.log(newWidth); 
         }
-        width++
-        const progressFill = document.getElementById('progress-fill');
-        // const btn = e.target;
-        progressFill.style.width = width + e.target.dataset +  "%";
-        // btn.classList.toggle('active');
+        e.stopPropagation();
+    }
+}
+const updateProgressBar = () => {
+    console.log("progress bar");         
+    if(e.target !== e.currentTarget) {
+        const currentPledge = document.querySelector('.pledge-btn');
+        if(currentPledge) {
+            const progressFill = document.getElementById('progress-fill');
+            progressFill.style.width = pledgeTotal;
+            let newWidth = Number(e.target.dataset.value += pledgeTotal);
+            progressFill.style.width = (newWidth) + "px";
+        }
         e.stopPropagation();
     }
 }
 
-window.addEventListener('click', selectPledgeAmount, false)
+window.addEventListener('click', selectPledgeAmount, updateProgressBar, false)
