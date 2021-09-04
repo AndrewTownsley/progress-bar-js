ec79d10e0ac91c239b613f9717686a0e6e6cdce7
Andrew TOwnsley <atownsley86@gmail.com>
2021-09-04 11:36:43 -0600

    Still working on getting the custom pledge form to hide when the next pledge btn is clicked, instead of having to reclick the pledge btn

--- app.js
+++ app.js
@@ -13,43 +13,48 @@ let pledgeCountValue = Number(localStorage.getItem('pledgeValue'));
 pledgeCount.innerText = `${pledgeCountValue} of`;
 
 
-
 const updatePledgeTotal = (e) => {
+    const activePledge = document.querySelector('.custom-pledge-form.active')
+    console.log(activePledge);
     console.log(e.target.nextSibling.nextSibling);
-    // const customPledgeForm = document.querySelectorAll('.custom-pledge-form');
-    e.target.nextSibling.nextSibling.classList.toggle('active');
-    // customPledgeForm.classList.toggle('active');
-    // ---------------------progress bar 
-    const progressFill = document.getElementById('progress-fill');
-    let pledgeValue = parseInt(e.target.dataset.value);
-    progressFill.style.width = parseInt(pledgeTotal);
-    let newWidth = pledgeTotal += pledgeValue;
-    progressFill.style.width = newWidth + "px";   
-    localStorage.setItem('newWidthStored', newWidth)
-    //-------------pledge count & backer count -----------------------
-    let backersCountDisplay = document.getElementById('backersCount');
-    backersCount++;
-    backersCountDisplay.innerText = backersCount; 
-    const pledgeCount = document.getElementById('pledgeCount');
-    let pledgeBtnValue =  parseInt(e.target.dataset.value)
-    let pledgeCountValue = pledgeCounter += pledgeBtnValue; 
-    pledgeCount.innerText = `${pledgeCountValue} of`;
-    localStorage.setItem("pledgeValue", pledgeCountValue);
-    console.log(pledgeCountValue);
-    if(pledgeCountValue >= 500) {
-        btnContainer.classList.add('disabled');
-        goalContainer.classList.add('complete');
+    if(e.target.nextSibling.nextSibling === activePledge){
+        activePledge.classList.toggle('active');
+    } else {
+        
+        e.target.nextSibling.nextSibling.classList.toggle('active');
+        // customPledgeForm.classList.toggle('active');
+        // ---------------------progress bar 
+        const progressFill = document.getElementById('progress-fill');
+        let pledgeValue = parseInt(e.target.dataset.value);
+        progressFill.style.width = parseInt(pledgeTotal);
+        let newWidth = pledgeTotal += pledgeValue;
+        progressFill.style.width = newWidth + "px";   
+        localStorage.setItem('newWidthStored', newWidth)
+        //-------------pledge count & backer count -----------------------
+        let backersCountDisplay = document.getElementById('backersCount');
+        backersCount++;
+        backersCountDisplay.innerText = backersCount; 
+        const pledgeCount = document.getElementById('pledgeCount');
+        let pledgeBtnValue =  parseInt(e.target.dataset.value)
+        let pledgeCountValue = pledgeCounter += pledgeBtnValue; 
+        pledgeCount.innerText = `${pledgeCountValue} of`;
+        localStorage.setItem("pledgeValue", pledgeCountValue);
+        console.log(pledgeCountValue);
+        if(pledgeCountValue >= 500) {
+            btnContainer.classList.add('disabled');
+            goalContainer.classList.add('complete');
+        }
+        }
     }
-}
-
-// const setLocalStorage = () => {
-// }
-
-
-pledgeBtns.forEach(pledge => {
-    pledge.addEventListener('click', updatePledgeTotal)
-})
-
+    
+    // const setLocalStorage = () => {
+        // }
+        
+        
+        pledgeBtns.forEach(pledge => {
+            pledge.addEventListener('click', updatePledgeTotal)
+        })
+        
 
 // selectPledgeFunc = () => {
 //     console.log("pledgeFunc");
